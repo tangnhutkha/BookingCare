3b6b138a8cc90fa79302ebe098ca634defa7fafe
Kha Tang <tangnhutkha209@gmail.com>
2023-05-18 12:08:33 +0700

    #65 markdown editor

---
+++ src/migrations/migration-create-markdown.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('markdowns', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            contentHTML: {
+                allowNull: false,
+                type: Sequelize.TEXT('long')
+            },
+            contentMarkdown: {
+                allowNull: false,
+                type: Sequelize.TEXT('long')
+            },
+            description: {
+                allowNull: true,
+                type: Sequelize.TEXT('long')
+            },
+            doctorId: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            specialtyId: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            clinicId: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: true,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: true,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('markdowns');
+    }
+};
